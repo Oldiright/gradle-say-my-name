da54924205ad01fafd7c7dc156a2a32fbff003c6
oldiright <oliynuk_Dima@ukr.net>
2023-11-12 20:28:50 +0200

    User dto created;

--- src/main/java/org/example/dto/User.java
+++ src/main/java/org/example/dto/User.java
@@ -1,4 +1,11 @@
 package org.example.dto;
 
 public class User {
+    private String name;
+    private String lastName;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
 }

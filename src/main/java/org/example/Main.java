f5aac4962bc14a3ecbb54e1a0fc1b104393e0c16
oldiright <oliynuk_Dima@ukr.net>
2023-11-12 20:53:45 +0200

    add gson in gradle.kts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.example.dto.User;
+
 public class Main {
     public static void main(String[] args) {
-
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        User user = new User("Dmytro", "Oliinyk");
+        System.out.println(gson.toJson(user));
 
     }
 }
